d7836f7995cf74e57793abc5b9c8d8cd9b4d8ac4
thdusqpf35 <@naver.com>
2024-02-24 11:04:24 +0900

    update

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Navigation from "./src/navigations";
 import { generateClient } from 'aws-amplify/api';
 import {
